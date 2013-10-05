4fad7a0b2d45e403af728d1edcd9100a28ff6b98
Shane Rogers <swrogers@gmail.com>
2013-10-05 10:49:00 -0500

    Added BloggerBlogBundle and initial templates

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ My Symblog Repository
 
 This will be used to hold my work through of the [Symblog Tutorial] [1]
 
+h3: Differences between my version and the symblog version
+
+* I will be using annotations
+
 [1]: http://tutorial.symblog.co.uk
---
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class PageController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+      return array(
+                   'notaname' => 'Not a name',
+                   );
+    }
+
+}
